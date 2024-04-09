e366c514bad3a722d8b1e178d8718bab2246d62f
anunezes <antonionunezescamilla@gmail.com>
2024-04-09 11:51:38 +0200

    fix enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   firebase: {
     databaseUrl: 'https://getreadyforai-default-rtdb.europe-west1.firebasedatabase.app/'
   },
+  urlback: 'https://greencrossback-end.onrender.com/api/user/',
   production: true
 };
